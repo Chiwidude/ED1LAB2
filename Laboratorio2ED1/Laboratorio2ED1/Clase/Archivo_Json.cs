012b25e5cfd2462da631b0da22f4fa625f246bbf
angel <angel@HP-ANGEL>
2018-03-08 07:24:03 -0600

    Archivo Json

---
+++ Laboratorio2ED1/Laboratorio2ED1/Clase/Archivo_Json.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using ArbolBinarioBu;
+using System.IO;
+using Newtonsoft.Json;
+
+namespace Laboratorio2ED1.Clase
+{
+    public class Archivo_Json<T>
+    {
+        public Nodo<T> Dato(Stream ruta)
+        {
+            try
+            {
+                Nodo<T> _aux;
+                StreamReader lector1 = new StreamReader(ruta);
+                string Informacion = lector1.ReadToEnd();
+                _aux = JsonConvert.DeserializeObject<Nodo<T>>(Informacion);
+                lector1.Close();
+                return _aux;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+    }
+}
